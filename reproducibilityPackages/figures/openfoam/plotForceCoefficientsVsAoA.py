6cd6fffe4658d122b9fec9529adbbf423d5e7059
mesnardo <mesnardo@gwu.edu>
2016-08-10 10:59:03 -0400

    Clean-up scripts to generate the OpenFOAM figures
    The folder contains 4 Python scripts, one for each figure about OpenFOAM
    in the manuscript.
    
    The script `plotVorticity.py` creates two files:
    * `openfoam_vorticity52Re2000AoA35_gmshZeroGradient.png`: vorticity
    field after 52 time-units of flow-simulation when using a GMSH mesh and
    a zero-gradient outlet boundary condition;
    * `openfoam_vorticity52Re2000AoA35_gmshZeroGradient.pdf`: the .pdf version
    (the one that should be included in the manuscript).
    
    The script `plotPressure.py` creates 3 files:
    * `openfoam_pressure52Re2000AoA35_gmshZeroGradient.png`: pressure field after
    52 time-units of flow-simulation when using a GMSH mesh and a zero-gradient
    outlet condition;
    * `openfoam_pressure53Re2000AoA35_gmshZeroGradient.png`: same than above with
    the solution after 53 time-units;
    * `openfoam_pressureRe2000AoA35_gmshZeroGradient.pdf`: 1x2 assembly of the two
    .png files above into a single .pdf file (the one that should go in the
    manuscript).
    
    The script `plotForceCoefficientsVsAoA.py` creates 2 files:
    * `openfoam_forceCoefficientsVsAoA.png`: time-averaged force coefficients with a
    comparison to the results reported in Krishnan et al. (2014);
    * `openfoam_forceCoefficientsVsAoA.pdf`: the .pdf version (the one to go in the
    manuscript).
    
    The script `plotForceCoefficientsRe2000.py` creates 3 files:
    * `openfoam_forceCoefficientsRe2000AoA30.png`: instantaneous force
    coefficients at Re=2000 and AoA=30deg with comparison to the force coefficients
    obtained with cuIBM by Krishnan et al. (2014);
    * `openfoam_forceCoefficientsRe2000AoA35.png`: same than above except at
    AoA=35deg;
    * `openfoam_forceCoefficientsRe2000.pdf`: 1x2 assembly of the 2 .png files
    above into a single .pdf file (the one to go in the manuscript).
    
    The folder also contains a YAML that should be used to list the directory of
    each OpenFOAM simulations used to generate the figures.
    
    I removed the unecessary log files and bash scripts.

--- reproducibilityPackages/figures/openfoam/plotForceCoefficientsVsAoA.py
+++ reproducibilityPackages/figures/openfoam/plotForceCoefficientsVsAoA.py
@@ -1,58 +1,74 @@
-# file: plotMeanForceCoefficientsVsAoACompareKrishnanEtAl2014.py
-# author: Olivier Mesnard (mesnardo@gwu.edu)
-# description: Computes and plots the mean force coefficients 
-#              versus the angle of attack 
-#              and compares with results from Krishnan et al. (2014).
-
+"""
+Generates Figure 3 of the manuscript 
+as a .pdf file named `openfoam_forceCoefficientsVsAoA.pdf`.
+"""
 
 import os
-import sys
+import yaml
+import shutil
+import argparse
 
 from matplotlib import pyplot
-from matplotlib import gridspec
 
 import snake
 from snake.openfoam.simulation import OpenFOAMSimulation
 from snake.cuibm.simulation import CuIBMSimulation
 
 
-print('\nPython version:\n{}'.format(sys.version))
-print('\nsnake version: {}\n'.format(snake.__version__))
+if snake.__version__ != '0.1.1':
+  warnings.warn('The figures were originally created with snake-0.1.1, '+
+                'you are using snake-{}'.format(snake.__version__))
 
-aoa = [25, 30, 35, 40]
 
-# compute mean coefficients of each OpenFOAM simulations
-cd, cl = [], []
-directory = os.path.join(os.environ['HOME'], 'simulations_OpenFOAM',
-                         'flyingSnake2d', 'snappyHexMesh', 
-                         'surfaceFeatureExtract')
-directory = os.path.join(os.environ['HOME'],
-                         'snakeReproducibilityPackages',
-                         'openfoam',
-                         'snappyHexMesh')
-folders = ['Re{}AoA{}'.format(re, a) for re in [1000, 2000] for a in aoa]
-for folder in folders:
-  simulation = OpenFOAMSimulation(directory=os.path.join(directory, folder))
-  simulation.read_forces(display_coefficients=True)
-  simulation.get_mean_forces(limits=[32.0, 64.0])
-  cd.append(simulation.forces[0].mean['value'])
-  cl.append(simulation.forces[1].mean['value'])
+# initialization
+script_directory = os.path.dirname(os.path.realpath(__file__))
+parser = argparse.ArgumentParser()
+parser.add_argument('--map', 
+                    dest='map', 
+                    type=str, 
+                    default=os.path.join(script_directory, 'map.yaml'), 
+                    help='file containing the list of simulation directories')
+parser.add_argument('--save-dir', 
+                    dest='save_directory', 
+                    type=str, 
+                    default=script_directory, 
+                    help='directory where to save the figures')
+args = parser.parse_args()
+with open(args.map, 'r') as infile:
+  dirs = yaml.load(infile)
 
-# compute mean coefficients of each simulations from Krishnan et al. (2014)
+
+# Computes the mean coefficients from the OpenFOAM simulations.
+# The force coefficients are averaged between 32 and 64 time-units.
+cd_openfoam, cl_openfoam = [], []
+for re in ['Re1000', 'Re2000']:
+  for aoa in ['AoA25', 'AoA30', 'AoA35', 'AoA40']:
+    simulation_directory = dirs['openfoam_forceCoefficientsVsAoA'][re][aoa]
+    simulation = OpenFOAMSimulation(directory=simulation_directory)
+    simulation.read_forces(display_coefficients=True)
+    simulation.get_mean_forces(limits=[32.0, 64.0])
+    cd_openfoam.append(simulation.forces[0].mean['value'])
+    cl_openfoam.append(simulation.forces[1].mean['value'])
+
+# Computes the mean coefficients from the cuIBM simulations
+# reported in Krishnan et al. (2014).
+# The force coefficients are averaged between 32 and 64 time-units.
 cd_krishnan, cl_krishnan = [], []
-for Re in [1000, 2000]:
-  for a in aoa:
-    directory = os.path.join(os.environ['SNAKE'],
-                             'resources',
-                             'flyingSnake2d_cuibm_anush',
-                             'flyingSnake2dRe{}AoA{}'.format(Re, a))
-    krishnan = CuIBMSimulation(directory=directory)
+for re in ['Re1000', 'Re2000']:
+  for aoa in ['AoA25', 'AoA30', 'AoA35', 'AoA40']:
+    simulation_directory = os.path.join(os.environ['SNAKE'],
+                                        'resources',
+                                        'flyingSnake2d_cuibm_anush',
+                                        'flyingSnake2d'+re+aoa)
+    krishnan = CuIBMSimulation(directory=simulation_directory)
     krishnan.read_forces()
     krishnan.get_mean_forces(limits=[32.0, 64.0])
     cd_krishnan.append(2.0*krishnan.forces[0].mean['value'])
     cl_krishnan.append(2.0*krishnan.forces[1].mean['value'])
 
-# plot mean drag coefficient versus angle of attack for Re=1000 and Re=2000
+
+# plot figure
+aoa  = [25, 30, 35, 40]
 pyplot.style.use(os.path.join(os.environ['SNAKE'], 
                               'snake', 
                               'styles', 
@@ -69,7 +85,7 @@ gs.update(wspace=0.5, hspace=0.1)
 ax1.grid(True, zorder=0)
 ax1.set_ylabel('drag coefficient')
 # Re=1000
-ax1.plot(aoa, cd[:len(aoa)],  
+ax1.plot(aoa, cd_openfoam[:len(aoa)],  
          label='IcoFOAM',
          color='#1B9E77', 
          linestyle='-', 
@@ -86,7 +102,7 @@ ax1.plot(aoa, cd_krishnan[:len(aoa)],
          markerfacecolor='none',
          zorder=10)
 # Re=2000
-ax1.plot(aoa, cd[len(aoa):],  
+ax1.plot(aoa, cd_openfoam[len(aoa):],  
          label='IcoFOAM',
          color='#D95F02', 
          linestyle='-', 
@@ -110,12 +126,12 @@ ax2.grid(True, zorder=0)
 ax2.set_xlabel('angle of attack')
 ax2.set_ylabel('lift coefficient')
 # Re=1000
-ax2.plot(aoa, cl[:len(aoa)],  
+ax2.plot(aoa, cl_openfoam[:len(aoa)],  
          label='IcoFOAM',
          color='#1B9E77', 
          linestyle='-', 
          marker='o',
-         zorder=12)
+         zorder=11)
 ax2.plot(aoa, cl_krishnan[:len(aoa)],  
          label='Krishnan et al. (2014)',
          color='#666666', 
@@ -127,12 +143,12 @@ ax2.plot(aoa, cl_krishnan[:len(aoa)],
          markerfacecolor='none',
          zorder=10)
 # Re=2000
-ax2.plot(aoa, cl[len(aoa):],  
+ax2.plot(aoa, cl_openfoam[len(aoa):],  
          label='IcoFOAM',
          color='#D95F02',
          linestyle='-', 
          marker='s',
-         zorder=12)
+         zorder=11)
 ax2.plot(aoa, cl_krishnan[len(aoa):],  
          label='Krishnan et al. (2014)',
          color='#666666', linestyle='--', 
@@ -149,22 +165,29 @@ ax2.set_xticklabels(aoa_labels)
 # create legend
 handles, labels = ax1.get_legend_handles_labels()
 ax3.axis('off')
-legend = ax3.legend(handles[:len(handles)/2], 
-                    labels[:len(labels)/2], 
+legend = ax3.legend(handles[:2], 
+                    labels[:2], 
                     title='Re = 1000',
                     loc='lower left',
-                    prop={'size': 9})
+                    prop={'size': 9},
+                    scatterpoints=1)
 legend.get_title().set_fontsize('9')
 ax4.axis('off')
-legend = ax4.legend(handles[len(handles)/2:], 
-                    labels[len(labels)/2:], 
+legend = ax4.legend(handles[2:], 
+                    labels[2:], 
                     title='Re = 2000',
                     loc='lower right',
-                    prop={'size': 9})
+                    prop={'size': 9},
+                    scatterpoints=1)
 legend.get_title().set_fontsize('9')
-pyplot.savefig('openfoam_forceCoefficientsVsAoA.pdf',
+# save figure in .png and .pdf formats
+file_path_out = os.path.join(args.save_directory, 
+                             'openfoam_forceCoefficientsVsAoA.pdf')
+pyplot.savefig(file_path_out,
                bbox_inches='tight',
-               format='pdf')
-pyplot.savefig('openfoam_forceCoefficientsVsAoA.png',
+               format='pdf',
+               dpi=300)
+pyplot.savefig(file_path_out.replace('.pdf', '.png'),
                bbox_inches='tight',
-               format='png')
+               format='png',
+               dpi=300)
